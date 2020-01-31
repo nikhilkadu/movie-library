cc6ccebd7116a5d853c3537d798cc9a4357c3f29
Nikhil Kadu <nikhil.kadu@gmail.com>
2020-01-31 09:53:30 +0530

    Display sort icon. Make table header labels clickable

--- src/components/common/TableHeader.jsx
+++ src/components/common/TableHeader.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class TableHeader extends Component {
   setSortColumn = clickedColumn => {
     const { sortColumn, onSort } = this.props;
+    if (!clickedColumn) return null;
     if (sortColumn.id === clickedColumn) {
       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
     } else {
@@ -11,16 +12,26 @@ class TableHeader extends Component {
     }
     onSort(sortColumn);
   };
+
+  showSortIcon(column) {
+    const { sortColumn } = this.props;
+    if (column.id !== sortColumn.id) return null;
+    if (sortColumn.order === "desc")
+      return <i className="fa fa-sort-desc" aria-hidden="true"></i>;
+    return <i className="fa fa-sort-asc" aria-hidden="true"></i>;
+  }
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map(column => (
             <th
+              className={column.label ? "clickable" : ""}
               key={column.id || column.key}
               onClick={() => this.setSortColumn(column.id)}
             >
-              {column.label}
+              {column.label} {this.showSortIcon(column)}
             </th>
           ))}
         </tr>
